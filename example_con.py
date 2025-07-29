fceede0c7014b7eb16d840f142dc3a5d40277ed5
AsliOzdemirStrollo <aslistrollo@gmail.com>
2025-07-29 17:42:06 +0200

    Update example_con.py

--- example_con.py
+++ example_con.py
@@ -1,14 +1,1 @@
-import streamlit as st
-from sqlalchemy import create_engine
-
-schema = "Lianas_Library"
-port = 3306
-
-# Get sensitive info from Streamlit secrets
-host = st.secrets["DB_HOST"]
-user = st.secrets["DB_USER"]
-password = st.secrets["DB_PASS"]
-
-# Create SQLAlchemy engine string dynamically
-module_string = f'mysql+pymysql://{user}:{password}@{host}:{port}/{schema}'
-engine = create_engine(module_string)
+#
